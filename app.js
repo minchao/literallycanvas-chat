c2bfe94a58db0a6fce151006df86f1080c4b16fe
minchao <minchao.220@gmail.com>
2017-03-07 22:15:18 +0800

    Add the save/load shapes functions

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http')
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const app = express()
 const server = http.createServer(app)
 const io = require('socket.io')(server)
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
+const tempFile = 'temp.json';
 
 (function () {
   if (process.env.NODE_ENV !== 'development') {
@@ -40,6 +42,22 @@ app.use('/static', express.static('public'))
 
 let shapes = []
 
+function saveShapesToFile () {
+  fs.writeFile(tempFile, JSON.stringify(shapes), 'utf8')
+}
+
+function loadShapesFromFile () {
+  fs.readFile(tempFile, 'utf8', (err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      shapes = JSON.parse(data)
+    }
+  })
+}
+
+loadShapesFromFile()
+
 io.on('connection', function (socket) {
   socket.on('init', function () {
     // sync shapes history
@@ -54,8 +72,14 @@ io.on('connection', function (socket) {
   })
 
   socket.on('chat', function (message) {
-    message.user = socket.id
-    socket.broadcast.emit('chat', message)
+    switch (message.text) {
+      case '!save':
+        saveShapesToFile()
+        break
+      default:
+        message.user = socket.id
+        socket.broadcast.emit('chat', message)
+    }
   })
 
   socket.on('disconnect', function () {
